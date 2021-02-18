6c7f2d4c0ce9f0a6d642c098e6b49e694fe88f5c
Rok Goljat <rok.goljat@gmail.com>
2021-02-18 23:32:19 +0100

    Initial commit of first working version

---
+++ Assets/Scripts/ButtonClickREST.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Net;
+using System;
+using System.IO;
+using UnityEngine.Events;
+using UnityEngine.Networking;
+
+// sources: 
+// https://stackoverflow.com/questions/36239705/serialize-and-deserialize-json-and-json-array-in-unity
+// https://stackoverflow.com/questions/45229260/unity-json-de-serializing-nested-data
+
+public class ButtonClickREST : MonoBehaviour
+{
+    // Public variables
+    public GameObject houseBase;
+    public VillageInfo[] village;
+
+    // Private variables
+    Vector3 coordinates = new Vector3(0,0,0);
+
+    // Update is called once per frame
+    public void OnMouseUp(string text)
+    {
+        Text txt = transform.Find("Text").GetComponent<Text>();
+        txt.text = text;
+        Debug.Log("aasdad");
+        print("click0");
+    }
+
+    public void PopulateHouses()
+    {
+        // Querry JSON data
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("https://jsonplaceholder.typicode.com/users"));
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string jsonResponse = JsonHelper.fixJson(reader.ReadToEnd());
+               
+        // Deserialize data to lists
+        village = JsonHelper.FromJson<VillageInfo>(jsonResponse);
+
+        GameObject[] houseDescription = GameObject.FindGameObjectsWithTag("HouseTag");
+
+        // Prevent additional calls
+        if (houseDescription.Length == 0)
+        { 
+            foreach (VillageInfo house in village)
+            {
+                Debug.Log(house.address.zipcode);
+                coordinates.x = float.Parse(house.address.geo.lat);
+                coordinates.z = float.Parse(house.address.geo.lng);
+            
+                GameObject newHouse = Instantiate(houseBase, coordinates, Quaternion.identity) as GameObject;
+
+                // Ta klica delata v Imediate windowu, v kodi pa ne
+                //Text houseName = newHouse.Find("TextName").GetComponent<Text>() as Text;
+                //houseName.text = house.name;
+                // Dirty!
+                Text houseName = newHouse.transform.GetChild(1).GetChild(0).GetComponent<Text>();
+                houseName.text = house.name;
+            }
+        }
+    }
+
+    public static class JsonHelper
+    {
+        public static string fixJson(string value)
+        {
+            value = "{\"Items\":" + value + "}";
+            return value;
+        }
+
+        public static T[] FromJson<T>(string json)
+        {
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+            return wrapper.Items;
+        }
+
+        public static string ToJson<T>(T[] array)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper);
+        }
+
+        public static string ToJson<T>(T[] array, bool prettyPrint)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper, prettyPrint);
+        }
+
+        [Serializable]
+        private class Wrapper<T>
+        {
+            public T[] Items;
+        }
+    }
+}
+
+// Kopiraj Json vsebino -> Edit -> Paste special -> Paste JSON as classes
+//public class Rootobject
+//{
+//    public VillageInfo[] Property1 { get; set; }
+//}
+
+[Serializable]
+public class VillageInfo
+{
+    public int id;
+    public string name;
+    public string username;
+    public string email;
+    public Address address;
+    public string phone;
+    public string website;
+    public Company company;
+}
+
+[Serializable]
+public class Address
+{
+    public string street;
+    public string suite;
+    public string city;
+    public string zipcode;
+    public Geo geo;
+}
+
+[Serializable]
+public class Geo
+{
+    public string lat;
+    public string lng;
+}
+
+[Serializable]
+public class Company
+{
+    public string name;
+    public string catchPhrase;
+    public string bs;
+}
---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+
+public class CameraControl : MonoBehaviour
+{
+    // Public variables
+    public Transform target;
+    public GameObject DatabaseObject;
+
+    // Private variables
+    private ButtonClickREST databaseScript;
+
+    // Translation speed
+    public int translationSpeed = 40;
+    // Rotation speed
+    public float rotationSpeed = 1.5f;
+    // Dead zone for mouse rotations
+    int deadzone = 1;
+    // Speed limit for rotations
+    int speedlimit = 5;
+    // Limit movement area
+    int maxPosition = 100;
+    // timer for mouseover
+    float timer = 0;
+    float timeLimit = 5;
+
+    // Placeholders
+    float drX, drY = 0f;
+    Vector3 localY = new Vector3(0, 0, 0);
+    Vector3 currentRotation = new Vector3(0, 0, 0);
+    Vector3 currentPosition = new Vector3(0, 0, 0);
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Controller definitions
+        var kbd = Keyboard.current;
+        var mouse = Mouse.current;
+
+        // If left arrow key is pressed
+        if (kbd.leftArrowKey.isPressed)
+        {
+            // move camera left
+            transform.Translate(Vector3.left * Time.deltaTime * translationSpeed);
+        }
+
+        // If right arrow key is pressed
+        if (kbd.rightArrowKey.isPressed)
+        {
+            // move camera right
+            transform.Translate(Vector3.right * Time.deltaTime * translationSpeed);
+        }
+
+        // If up arrow key is presed
+        if (kbd.upArrowKey.isPressed)
+        {
+            // Get camera forward direction without "UP" component
+            localY = transform.forward;
+            localY.y = 0;
+            // Move camera forward
+            transform.Translate(localY * Time.deltaTime * translationSpeed, Space.World);
+        }
+
+        // if down arrow key is pressed
+        if (kbd.downArrowKey.isPressed)
+        {
+            // Get forward direction without "UP" component
+            localY = transform.forward;
+            localY.y = 0;
+            // Move camera backward
+            transform.Translate(-localY * Time.deltaTime * translationSpeed, Space.World);
+        }
+
+        // Limit camera position
+        if (kbd.anyKey.isPressed)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -maxPosition, maxPosition), transform.position.y, Mathf.Clamp(transform.position.z, -maxPosition, maxPosition));
+        }
+
+
+        // Do rotations if RMB is pressed
+        if (mouse.rightButton.isPressed)
+        {
+            // read mouse movement
+            drX = Mathf.Clamp(mouse.delta.x.ReadValue(), -speedlimit, speedlimit);
+            drY = Mathf.Clamp(mouse.delta.y.ReadValue(), -speedlimit, speedlimit);
+
+            // If mouse is moved verticaly
+            if (Mathf.Abs(drY) >= deadzone)
+            {
+                // Do this in world space to prevent tilts
+                transform.Rotate(-drY * rotationSpeed, 0, 0);
+                // Limit rotations
+                currentRotation = transform.localRotation.eulerAngles;
+                if (currentRotation.x < 180)
+                {
+                    currentRotation.x = Mathf.Clamp(currentRotation.x, 0, 45);
+                }
+                else
+                {
+                    currentRotation.x = Mathf.Clamp(currentRotation.x, 355, 360);
+                }
+                transform.localRotation = Quaternion.Euler(currentRotation);
+            }
+
+            // If mouse is moved horizontaly
+            if (Mathf.Abs(drX) >= deadzone)
+            {
+                // Do this in world space to prevent tilts
+                transform.Rotate(0, drX * rotationSpeed, 0, Space.World);
+                // No limit for Y axis rotation
+            }
+        }
+
+
+        // Make all canvases look at camera
+        GameObject[] houseDescription = GameObject.FindGameObjectsWithTag("HouseTag");
+        foreach (GameObject tag in houseDescription)
+        {
+            tag.transform.LookAt(target);
+        }
+
+        // On click add description
+        if (mouse.leftButton.isPressed)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(mouse.position.ReadValue());
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // If house is clicked
+                if (hit.transform.tag == "HouseTag")
+                {
+                    // Probably not the best way to add descriptions...
+                    Text houseDescr = hit.transform.GetChild(1).GetChild(1).GetComponent<Text>();
+                    Text houseName = hit.transform.GetChild(1).GetChild(0).GetComponent<Text>();
+
+                    // Only add description if none exists
+                    if (houseDescr.text == "")
+                    {
+                        // Acces database
+                        databaseScript = DatabaseObject.GetComponent<ButtonClickREST>();
+                        foreach (VillageInfo house in databaseScript.village)
+                        {
+                            if (house.name.Equals(houseName.text))
+                            {
+                                houseDescr.text = house.company.catchPhrase;
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+        // If mouse is still
+        if (mouse.delta.x.ReadValue() == 0 && mouse.delta.y.ReadValue() == 0)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(mouse.position.ReadValue());
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // If its hovering over a house
+                if (hit.transform.tag == "HouseTag")
+                {
+                    timer += Time.deltaTime;
+                    if (timer >= timeLimit)
+                    {
+                        // Probably not the best way to add descriptions...
+                        Text houseDescr = hit.transform.GetChild(1).GetChild(1).GetComponent<Text>();
+                        Text houseName = hit.transform.GetChild(1).GetChild(0).GetComponent<Text>();
+
+                        // Only add description if none exists
+                        if (houseDescr.text == "")
+                        {
+                            // Acces database
+                            databaseScript = DatabaseObject.GetComponent<ButtonClickREST>();
+                            foreach (VillageInfo house in databaseScript.village)
+                            {
+                                if (house.name.Equals(houseName.text))
+                                {
+                                    houseDescr.text = house.company.catchPhrase;
+                                }
+                            }
+
+                        }
+                    }
+
+                }
+            }
+        }
+        else
+        {
+            timer = 0;
+        }
+
+    }
+}
